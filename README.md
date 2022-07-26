685c5c183d6f78b554016a24f27fad7fd2852c78
dvh549 <d.v.h549@hotmail.com>
2022-07-27 00:31:53 +0800

    Running deep.py directly
    - app.py a template.
    - Updated readme accordingly.
    - Renamed model filename.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Share Price Prediction #
+# Share Price Prediction
 
 1. Share price information retrieved from Yahoo Finance.
-2. A deep learning model is used to predict the specified periods ("forecast_period") in "config_dict".
-3. Running a Flask app and calling endpoint ("/getPredictions") will return the predicted price(s).
+2. A deep learning model is used to predict the specified periods ("forecast_period").
+3. Running **deep.py** will prompt the user for the ticker and periods to forecast for.
+4. Once inputs are processed, it will return a list of predictions.
+
+## To notes
+1. Forecasted period is inclusive of the current day **IF** the market has not closed.
+2. **app.py** serves just as a template to integrate the model for a web application.
--- deep.py
+++ deep.py
@@ -1,7 +1,13 @@
+# import pdr required libs
+from pandas_datareader import data as pdr
+from datetime import datetime, timedelta
+import yfinance as yf
+yf.pdr_override()
+
 # import DL required libs
 import numpy as np
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import LSTM, Dense, Bidirectional, Dropout
+from tensorflow.keras.layers import LSTM, Dense
 
 class Deep:
     def __init__(self, config_dict):
@@ -28,21 +34,21 @@ class Deep:
         X, self.y = np.array(X), np.array(Y)
         self.X = X.reshape((X.shape[0], X.shape[1], 1))
 
+    def save_model(self):
+        self.model.save("saved_models/model.h5")
+
     def load_model(self):
-        self.model = load_model("saved_models/vanilla_lstm.h5")
+        self.model = load_model("saved_models/model.h5")
 
     def build(self):
         model = Sequential()
-        model.add(Bidirectional(LSTM(50, activation='relu', return_sequences=True), input_shape=(self.n_steps, self.n_features)))
-        model.add(Dropout(0.1))
-        model.add(Bidirectional(LSTM(50, activation="relu")))
-        model.add(Dropout(0.1))
+        model.add(LSTM(20, activation='relu', input_shape=(self.n_steps, self.n_features)))
         model.add(Dense(1))
         model.compile(optimizer="adam", loss="mae")
         self.model = model
 
     def train(self):
-        self.model.fit(self.X, self.y, epochs=10, verbose=0)
+        self.model.fit(self.X, self.y, epochs=150, batch_size=32, verbose=0)
 
     def predict(self):
         predictions = []
@@ -53,3 +59,21 @@ class Deep:
             predictions.append(float(yhat))
             x_input = np.append(x_input[1:], [yhat])
         return predictions
+
+    def run_steps_to_get_predictions(self):
+        self.load_config()
+        self._preprocess_data()
+        self.build()
+        self.train()
+        return self.predict()
+
+if __name__ == "__main__":
+    ticker = input("Which ticker would you like to predict for? ")
+    start, end = (datetime.today() - timedelta(days=365)).strftime("%Y-%m-%d"), datetime.today().strftime("%Y-%m-%d")
+    data = pdr.get_data_yahoo(ticker, start=start, end=end)
+    close = data["Close"]
+    forecast_period = input("How many days would you like to predict for? ")
+    config_dict = {"df": close, "n_steps": 24, "n_features": 1, "forecast_period": int(forecast_period)}
+    model = Deep(config_dict)
+    predictions = model.run_steps_to_get_predictions()
+    print(predictions)
