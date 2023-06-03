e2482c46a7a0ad61e926fdf254ce188672fa9409
dvh549 <d.v.h549@hotmail.com>
2023-06-04 04:00:47 +0800

    Java Connection Established

---
+++ ConnectToJava/Connect.java
@@ -0,0 +1,41 @@
+package ConnectToJava;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Connect {
+	public static void main(String[] args) {
+		Results();
+	}
+
+	public static void Results(){
+		try {
+	        URL url = new URL("http://localhost:5000/getPredictions");
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Accept", "application/json");
+	
+	        if (conn.getResponseCode() != 200) {
+	            throw new RuntimeException("Failed : HTTP error code : "
+	                    + conn.getResponseCode());
+	        }
+	
+	        BufferedReader br = new BufferedReader(new InputStreamReader(
+	                (conn.getInputStream())));
+	
+	        String output;
+	        System.out.println("Output from Server .... \n");
+	        while ((output = br.readLine()) != null) {
+	            System.out.println(output);
+	        }
+            
+	        conn.disconnect();
+	
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    } catch (IOException e){
+	    	e.printStackTrace();
+	    }
+	}
+}
